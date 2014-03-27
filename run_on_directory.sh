e9b732923d835f12fb222d0ae416a187d4ad2d4e
Simon Walker <s.r.walker101@googlemail.com>
2014-03-27 14:59:12 +0000

    Removes all file copying before image stacking

--- run_on_directory.sh
+++ run_on_directory.sh
@@ -25,32 +25,14 @@ setup_environment() {
     export LD_LIBRARY_PATH=/usr/local/wcslib/lib:/usr/local/cfitsio/lib:${LD_LIBRARY_PATH}
 }
 
-copy_files() {
-    OUTPUTDIR=$1
-    cp metadata.json catfile.fits catfile.ell outstack.fits outstackconf.fits stackfilelist ${OUTPUTDIR}
-}
-
 perform_analysis() {
     FILELIST=$1
     CONFIDENCE=$2
     OUTPUTDIR=$3
     PID=$$
-    
-    # Copy the source files onto local disk
-    TMPDIR="ngts-${PID}"
-    NEWFILELIST="filelist.txt"
-    (cd /tmp
-        test -d "${TMPDIR}" || mkdir "${TMPDIR}"
-        (cd "/tmp/${TMPDIR}"
-            echo "Copying files to `pwd`"
-            cp `cat ${FILELIST} | cut -d ' ' -f 1` .
-            find . -name '*.fits' > ${NEWFILELIST}
 
-            time ZLP_create_cat.py --confmap ${CONFIDENCE} --filelist ${NEWFILELIST} --verbose --create-ell --nproc ${NPROC}
-
-            copy_files "${OUTPUTDIR}"
-        )
-        rm -rf "${TMPDIR}"
+    (cd ${OUTPUTDIR}
+    time ZLP_create_cat.py --confmap ${CONFIDENCE} --filelist ${FILELIST} --verbose --create-ell --nproc ${NPROC}
     )
 }
 
