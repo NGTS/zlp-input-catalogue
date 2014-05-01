3a209e6243568673fe7d0ea70d6ba85b517c4c6c
Simon Walker <s.r.walker101@googlemail.com>
2014-05-01 13:42:29 +0100

    Strips out old code

--- ngts_catalogue/main.py
+++ ngts_catalogue/main.py
@@ -13,7 +13,6 @@ Options:
   -v, --verbose                             Print more text
   -o <OUTNAME>, --outname <OUTNAME>         Specify the name of the output catalog [default: catfile.fits]
   --c_thresh <C_THRESH>                     The detection threshold to use when defining the input [default: 7]
-  --s_thresh <S_THRESH>                     The detection threshold to use when WCS solving images typically higher than when doing actual photometry [default: 7]
   -n <NPROC>, --nproc <NPROC>               Enable multithreading if you're analysing a lot of files at once
   --outstack-name <outstack_name>           Output stack file name [default: outstack.fits]
   --outstackconf-name <outstackconf_name>   Output confidence map name [default: outstackconf.fits]
@@ -24,37 +23,14 @@ This is the catalog generation tool, requires a filelist input. need to work on 
 
 from docopt import docopt
 import os
-from .wcs_fitting import m_solve_images
-from . import casutools
-from .metadata import Metadata
-from .ngts_logging import logger
-from .version import __version__
 from tempfile import NamedTemporaryFile
 import sqlite3
 from datetime import datetime
 import tempfile
 
-def ensure_cache(filelist, cache_directory_name='catcache'):
-    cache_dir = os.path.join(
-            os.getcwd(), cache_directory_name)
-    if os.path.isdir(cache_dir):
-        logger.info('Cache directory found')
-    else:
-        logger.warning("No cache directory found, solving first image to cache data")
-        with open(filelist) as infile:
-            first_filename = infile.readline().strip('\n')
-        logger.warning('Using image {}'.format(first_filename))
-
-        with tempfile.NamedTemporaryFile(prefix='dummywcs-', suffix='.fits') as tfile:
-            name = tfile.name
-            logger.warning('Extracting sources from first image')
-            casutools.imcore(first_filename, name)
-
-            tfile.seek(0)
-
-            logger.warning('Finding dummy solution')
-            casutools.wcsfit(first_filename, name)
-
+from . import casutools
+from .ngts_logging import logger
+from .version import __version__
 
 def main():
     argv = docopt(__doc__, version=__version__)
@@ -64,15 +40,6 @@ def main():
     start = datetime.now()
 
     name = argv['--filelist']
-
-    if not argv['--no-wcs']:
-        logger.info("Astrometrically solving images")
-
-        ensure_cache(name)
-        extracted_metadata = m_solve_images(name, thresh=argv['--s_thresh'],
-                nproc=int(argv['--nproc']) if argv['--nproc'] else None,
-                confmap=argv['--confmap'])
-        Metadata(extracted_metadata).render()
 
     outstack_name = argv['--outstack-name']
     outstackconf_name = argv['--outstackconf-name']
